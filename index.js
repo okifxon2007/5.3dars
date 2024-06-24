5edea1ba020d00f4964634710d535778b6fa1c12
okifxon <shokirjonovokifxon46@gmail.com>
2024-06-24 13:49:21 +0500

    5.3dars

---
+++ index.js
@@ -0,0 +1,289 @@
+// 1masala
+
+// const students = [
+//     { name: 'Alice', age: 20, score: 85, status: 'active' },
+//     { name: 'Bob', age: 22, score: 92, status: 'inactive' }
+//   ];
+
+// let ring = students.map(function (value){
+//     let result = value.name + ':' + value.age;
+//     return result
+// })
+
+// console.log(ring);
+
+// 2masala
+
+// ishlayolmadim
+
+// 3masala
+// ishlayolmadim
+
+// 4masala
+// ishlayolmadim
+
+// 5masala
+
+// const students = [
+//     { name: 'Alice', age: 20, score: 85, status: 'active' },
+//     { name: 'John', age: 22, score: 92, status: 'inactive' },
+//     { name: 'Bob', age: 23, score: 88, status: 'active' }
+//   ];
+
+
+//   const filterr = students.filter((student) =>{
+//     return student.name == 'John';
+//   })
+//   console.log(filterr);
+
+
+// 6masala
+// const students = [
+//     { name: 'Alice', age: 20, score: 80, status: 'active' },
+//     { name: 'Bob', age: 22, score: 92, status: 'inactive' },
+//     { name: 'Charlie', age: 23, score: 85, status: 'active' }
+//   ];
+
+//   const fill = students.filter((ball) => {
+//     if (ball.score >= 85) {
+//         return ball.score
+//     }
+//   })
+//   console.log(fill);
+
+// 7masala
+// const students = [
+//     { name: 'Alice', age: 20, score: 85, status: 'active' },
+//     { name: 'Bob', age: 22, score: 92, status: 'inactive' },
+//     { name: 'Charlie', age: 23, score: 88, status: 'active' }
+//   ];
+
+//   let scor = students.filter((student) =>{
+    
+//         return student.status == 'active';
+    
+//   })
+//   console.log(scor);
+
+// 8masala
+
+// const students = [
+//     { name: 'Alice', age: 20, score: 85, status: 'active' },
+//     { name: 'Bob', age: 22, score: 92, status: 'inactive' },
+//     { name: 'Charlie', age: 17, score: 88, status: 'active' }
+//   ];
+
+
+//   let yosh = students.filter((yosh) =>{
+//     return yosh.age >= 18;
+//   })
+
+//   console.log(yosh);
+
+// 9masala
+
+// const students = [
+//     { name: 'Alice', age: 20, score: 85, status: 'active' },
+//     { name: 'Bob', age: 22, score: 92, status: 'inactive' },
+//     { name: 'Charlie', age: 23, score: 88, status: 'active' }
+//   ];
+
+//   let score = students.filter((score) =>{
+//          return score.score >= 90;
+//       })
+// console.log(score);
+
+// 10masala
+
+// const students1 = [
+//     { name: 'Alice', age: 20, score: 85, status: 'active' },
+//     { name: 'Bob', age: 22, score: 92, status: 'inactive' }
+//   ];
+//   const students2 = [
+//     { name: 'Charlie', age: 23, score: 88, status: 'active' },
+//     { name: 'David', age: 21, score: 90, status: 'inactive' }
+//   ];
+
+
+//   let student = students1.concat(students2)
+
+// console.log(student);
+
+
+
+// 11masala
+
+
+// qila olmadim
+
+
+// 12masala
+// const students = [
+//     { name: 'Alice', age: 20, score: 85, status: 'active' },
+//     { name: 'Bob', age: 22, score: 92, status: 'inactive' }
+//   ];
+
+//   let rose = students.map(function (value){
+//     return value.name.toUpperCase()
+//   })
+//   console.log(rose);
+
+
+// 13masala
+
+
+// olib qoyamiz
+
+
+// 14masala
+
+// const students = [
+//     { name: 'Alice', age: 20, score: 85, status: 'active' }
+//   ];
+
+//   students.forEach(function(value, index){
+//     console.log(Object.keys(value));
+//   })
+
+// 15masala
+
+
+
+//   function User (name, age, score, status){
+//     this.name = name;
+//     this.age = age;
+//     this.score = score;
+//     this.status = status;
+//   }
+
+//   let john = new User ('john', 40, 85, 'active');
+//   console.log(john);
+//   const students = [
+//     { name: 'Alice', age: 20, score: 85, status: 'active' },
+//     { name: 'Bob', age: 22, score: 92, status: 'inactive' }
+//   ];
+//   console.log(students);
+
+
+// 16masala
+
+// qila olmadim
+
+
+// 17masala
+
+// const students = [
+//     { name: 'Alice', age: 20, score: 85, status: 'active' },
+//     { name: 'Bob', age: 22, score: 92, status: 'inactive' },
+//     { name: 'Charlie', age: 19, score: 88, status: 'active' }
+//   ];
+
+
+//   let resultt = students.filter(function (value) {
+
+//     if (value.age >= 20) {
+//         return value
+//     }
+//   })
+//   console.log(resultt);
+
+
+
+// 18masala
+
+
+// const students = [
+//     { name: 'Alice', age: 20, score: 85, status: 'active' },
+//     { name: 'Bob', age: 22, score: 92, status: 'inactive' },
+//     { name: 'Charlie', age: 23, score: 88, status: 'active' }
+//   ];
+
+//   let rose = students.map(function (value){
+//         return value.status
+//       })
+//   console.log(rose);
+
+
+// 19masala
+// ishlolmadim
+
+
+
+// 20masala
+
+// const students = [
+//     { name: 'Alice', age: 20, score: 85, status: 'active' },
+//     { name: 'Bob', age: 22, score: 92, status: 'inactive' },
+//     { name: 'Charlie', age: 19, score: 88, status: 'active' }
+//   ];
+
+//   let rose = students.map(function (value){
+//     if (value.age > value.age) {
+//         return value.age
+//     }
+//   })
+//   console.log(rose);
+
+
+
+
+// string metodlari
+
+// 1
+// let arr = 'hello-world';
+
+// let result = arr.substring(0,5);
+// console.log(result);
+// 2
+
+// let err = 'hello-world'
+// let arr = err.toUpperCase();
+// console.log(arr);
+
+// let err = 'HELLO-WORLD'
+// let arr = err.toLowerCase();
+// console.log(arr);
+
+
+// 3
+
+// let arr = 'Hello world from JavaScript';
+
+// let res = arr.split('',15)
+// console.log(res);
+
+// 4
+// let arr = ['Hello world from JavaScript'];
+
+// let res = arr.join(",")
+// console.log(res);
+
+// 5
+
+
+// 6
+
+// let arr = '   Hello world from JavaScript    ';
+
+// let res = arr.trim()
+// console.log(res);
+
+// 7
+
+// let arr = 'Hello world from JavaScript';
+
+// let res = arr.charAt(10)
+// console.log(res);
+// 8
+
+// let arr = 'Hello, world';
+
+// let res = arr.includes()
+// console.log(res);
+
+// 9
+
+// let arr = 'Hello, world ';
+
+// let res = arr.startsWith();
+// console.log(res);
